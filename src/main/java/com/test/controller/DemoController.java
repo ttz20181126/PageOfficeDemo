6267592690740aad4808ca8d52af07a8f771583b
Taycen <taizhen.tan@getech.cn>
2021-10-26 16:29:12 +0800

    将test.doc和序列号作为在线文件拉取创建

--- src/main/java/com/test/controller/DemoController.java
+++ src/main/java/com/test/controller/DemoController.java
@@ -1,15 +1,14 @@
 package com.test.controller;
 
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Map;
-import java.util.Objects;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,11 +21,17 @@ import com.zhuozhengsoft.pageoffice.*;
 @RestController
 public class DemoController {
 	
-	@Value("${posyspath}") 
+	@Value("${posyspath:d:\\lic\\}")
 	private String poSysPath;
 	
-	@Value("${popassword}") 
-	private String poPassWord;
+	//@Value("${popassword}")
+	//private String poPassWord;
+
+	@Value("${txtPath}")
+	private String txtPath;
+
+	@Value("${docPath}")
+	private String docPath;
 
 	@RequestMapping("/hello")
 	public String test() {
@@ -47,16 +52,35 @@ public class DemoController {
 	 * @return
 	 */
 	@RequestMapping(value="/word", method=RequestMethod.GET)
-	public ModelAndView showWord(HttpServletRequest request, Map<String,Object> map){
+	public ModelAndView showWord(HttpServletRequest request, Map<String,Object> map) throws IOException {
 		
-		PageOfficeCtrl poCtrl=new PageOfficeCtrl(request);
+		PageOfficeCtrl poCtrl = new PageOfficeCtrl(request);
 		poCtrl.setServerPage("/poserver.zz");//设置服务页面
 		poCtrl.addCustomToolButton("保存","Save",1);//添加自定义保存按钮
 		//poCtrl.addCustomToolButton("盖章","AddSeal",2);//添加自定义盖章按钮
 		poCtrl.addCustomToolButton("提交","submit",2);//提交到文件
 		poCtrl.setSaveFilePage("/save");//设置处理文件保存的请求方法
+
 		//打开word
-		poCtrl.webOpen("d:\\lic\\test.doc",OpenModeType.docAdmin,"张三");
+		System.out.println("posyspath:" + poSysPath);
+		File folder = new File(poSysPath);
+		if(!folder.exists() && !folder.isDirectory()){
+			folder.mkdir();
+		}
+		String filePath = poSysPath + "test.doc";
+		File file = new File(filePath);
+		if(!file.exists()){
+			System.out.println("写出test.doc");
+			urlToFile(docPath,"D:\\lic\\test.doc");
+		}
+		String txtPath = poSysPath + "序列号.txt";
+		File txtFile = new File(txtPath);
+		if(!txtFile.exists()){
+			System.out.println("写出序列号.txt");
+			urlToFile(txtPath,"D:\\lic\\序列号.txt");
+		}
+
+		poCtrl.webOpen(poSysPath + "test.doc",OpenModeType.docAdmin,"张三");
 		map.put("pageoffice",poCtrl.getHtmlCode("PageOfficeCtrl1"));
 		
 		ModelAndView mv = new ModelAndView("Word");
@@ -66,7 +90,7 @@ public class DemoController {
 	@RequestMapping("/save")
 	public void saveFile(HttpServletRequest request, HttpServletResponse response){
 		FileSaver fs = new FileSaver(request, response);
-		fs.saveToFile("d:\\" + fs.getFileName());
+		fs.saveToFile("d:\\lic\\" + fs.getFileName());
 		System.out.println("~~~~~~~~~~~~~~~正在保存~~~~~~~~~~~~~~~~~");
 		fs.close();
 	}
@@ -99,15 +123,63 @@ public class DemoController {
 	 * 添加印章管理程序Servlet（可选）
 	 * @return
 	 */
-	@Bean
-    public ServletRegistrationBean servletRegistrationBean2() {
-		com.zhuozhengsoft.pageoffice.poserver.AdminSeal adminSeal = new com.zhuozhengsoft.pageoffice.poserver.AdminSeal();
-		adminSeal.setAdminPassword(poPassWord);//设置印章管理员admin的登录密码
-		adminSeal.setSysPath(poSysPath);//设置印章数据库文件poseal.db存放的目录
-		ServletRegistrationBean srb = new ServletRegistrationBean(adminSeal);
-		srb.addUrlMappings("/adminseal.zz");
-		srb.addUrlMappings("/sealimage.zz");
-		srb.addUrlMappings("/loginseal.zz");
-        return srb;// 
-    }
+	//@Bean
+    //public ServletRegistrationBean servletRegistrationBean2() {
+	//	com.zhuozhengsoft.pageoffice.poserver.AdminSeal adminSeal = new com.zhuozhengsoft.pageoffice.poserver.AdminSeal();
+	//	adminSeal.setAdminPassword(poPassWord);//设置印章管理员admin的登录密码
+	//	adminSeal.setSysPath(poSysPath);//设置印章数据库文件poseal.db存放的目录
+	//	ServletRegistrationBean srb = new ServletRegistrationBean(adminSeal);
+	//	srb.addUrlMappings("/adminseal.zz");
+	//	srb.addUrlMappings("/sealimage.zz");
+	//	srb.addUrlMappings("/loginseal.zz");
+    //    return srb;//
+    //}
+
+	public static String urlToFile(String urlPath,String fileName) throws IOException {
+
+		//String fileName = "D:\\lic\\test.doc";
+
+		File file = new File(fileName);
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (FileNotFoundException e) {
+				//log.info("在临时目录创建文件失败");
+				//throw new ServiceException(new SelfResultCode("创建文件失败"));
+				System.out.println("创建文件失败");
+			}
+		}
+		InputStream is = null;
+		OutputStream os = null;
+		try {
+			URL url = new URL(urlPath);
+			// 打开连接
+			URLConnection con = url.openConnection();
+			// 输入流
+			is = con.getInputStream();
+			// 1K的数据缓冲
+			byte[] bs = new byte[1024];
+			// 读取到的数据长度
+			int len;
+			// 输出的文件流
+			os = new FileOutputStream(file);
+			// 开始读取
+			while ((len = is.read(bs)) != -1) {
+				os.write(bs, 0, len);
+			}
+		} catch (IOException e) {
+			//log.info(e.getMessage());
+			//throw new ServiceException(new SelfResultCode("读取文件流输出到文件失败"));
+			System.out.println("读取文件流输出到文件失败");
+		}finally {
+			// 完毕，关闭所有链接
+			if(os != null){
+				os.close();
+			}
+			if(is != null){
+				is.close();
+			}
+		}
+		return fileName;
+	}
 }
