7196aa33fe4cae9bef20afd60d2ac24c5794f085
Taycen <taizhen.tan@getech.cn>
2022-01-04 14:04:33 +0800

    数据域书签渲染样式；给tag设置值；获取数据源值；新建文档

--- src/main/java/com/test/controller/DemoController.java
+++ src/main/java/com/test/controller/DemoController.java
@@ -59,7 +59,8 @@ public class DemoController {
 	 */
 	@RequestMapping(value="/word", method=RequestMethod.GET)
 	public ModelAndView showWord(HttpServletRequest request, Map<String,Object> map) throws IOException {
-		
+		System.out.println("contextPath:" + request.getContextPath());
+
 		PageOfficeCtrl poCtrl = new PageOfficeCtrl(request);
 		poCtrl.setServerPage("/poserver.zz");//设置服务页面
 		poCtrl.addCustomToolButton("保存","Save",1);//添加自定义保存按钮
@@ -91,25 +92,55 @@ public class DemoController {
 
 		WordDocument doc = new WordDocument();
 
-		//书签1
+		//书签1 设置颜色、赋值
 		DataRegion dataReg = doc.openDataRegion("PO_USERNAME");
 		dataReg.getShading().setBackgroundPatternColor(Color.pink);
+		dataReg.setValue("谈太振");
 		//书签2
 		DataRegion dataReg2 = doc.openDataRegion("PO_week");
 		dataReg2.getShading().setBackgroundPatternColor(Color.pink);
-		dataReg.setEditing(true);
+		dataReg2.setEditing(true);
+		dataReg2.setValue("星期五");
 
 		//tag去set值
 		DataTag deptTag = doc.openDataTag("{状态}");
 		deptTag.setValue("very good");//给DataTag对象赋值
 		deptTag.getFont().setColor(Color.GREEN);
 
+
+		/**
+		 * 给table设置书签，通过缩影对整个table操作。
+		 */
+		//DataRegion dataRegion = doc.openDataRegion("PO_regTable");
+		//打开table，openTable(index)方法中的index代表Word文档中table位置的索引，从1开始
+		//Table table = dataRegion.openTable(1);
+		//给table中的单元格赋值， openCellRC(int,int)中的参数分别代表第几行、第几列，从1开始
+		//table.openCellRC(3, 1).setValue("A公司");
+		//table.openCellRC(3, 2).setValue("开发部");
+		//table.openCellRC(3, 3).setValue("李清");
+		//插入一行，insertRowAfter方法中的参数代表在哪个单元格下面插入一个空行
+		//table.insertRowAfter(table.openCellRC(3, 3));
+		//table.openCellRC(4, 1).setValue("B公司");
+		//table.openCellRC(4, 2).setValue("销售部");
+		//table.openCellRC(4, 3).setValue("张三");
+
+
+
 		poCtrl.setWriter(doc);
 
 		//设置office编辑tool
 		//poCtrl.setOfficeToolbars(false);//设置office编辑tool
 
+		//poCtrl.setAllowCopy(false);//禁止拷贝
+		//文档打开后立即执行的js方法定义   可配合页面js禁用ctrl+s、打印预览等。功能按钮灰色。
+		poCtrl.setJsFunction_AfterDocumentOpened("AfterDocumentOpened");
+
 		poCtrl.webOpen(filePath,OpenModeType.docAdmin,"张三");
+
+
+		// http://148.70.201.120:8888/Samples5/    二、3、新建文件（以Word为例）/CreateWord  新建文档
+		//poCtrl.webCreateNew("张佚名", DocumentVersion.Word2003);
+
 
 		//提交权限，不可编辑。
 		//poCtrl.webOpen(filePath,OpenModeType.docSubmitForm,"张三");
@@ -123,8 +154,20 @@ public class DemoController {
 	@RequestMapping("/save")
 	public void saveFile(HttpServletRequest request, HttpServletResponse response){
 		FileSaver fs = new FileSaver(request, response);
+
+
+		//获取数据域（书签）的数据
+		//com.zhuozhengsoft.pageoffice.wordreader.WordDocument doc = new com.zhuozhengsoft.pageoffice.wordreader.WordDocument(request,response);
+		//com.zhuozhengsoft.pageoffice.wordreader.DataRegion dataUserName = doc.openDataRegion("PO_USERNAME");
+		//com.zhuozhengsoft.pageoffice.wordreader.DataRegion dataDeptName = doc.openDataRegion("PO_week");
+		//System.out.println("获取数据域-书签的数据为:" + dataUserName.getValue() + "," + dataDeptName.getValue());
+
 		fs.saveToFile("d:\\PageOffice_License\\" + fs.getFileName());
-		System.out.println("~~~~~~~~~~~~~~~正在保存~~~~~~~~~~~~~~~~~");
+		System.out.println("~~~~~~~~~~~~~~~已保存~~~~~~~~~~~~~~~~~");
+
+		//点击返回保存结果，ctrl+s无效，点击上方的“保存”按钮才有作用。
+		fs.setCustomSaveResult("success！");
+
 		fs.close();
 	}
 
